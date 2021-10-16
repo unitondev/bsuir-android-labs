ae1fa53d16fa3c12fb7b3e12b751bc7576318e70
Artyom Korbut <lovrovalx@gmail.com>
2021-10-16 04:39:01 +0300

    Lab1 done

---
+++ screens/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+const Home = props => (
+    <View>
+        <Text> hello world! </Text>
+    </View>
+)
+
+const styles = StyleSheet.create({
+
+})
+
+export default Home
